cf2167c74cefb77835b3a4c74a764177fafd4a0d
ksza <karoly.szanto@3pillarglobal.com>
2015-11-29 22:48:02 +0200

    Added butterknife

--- app/src/main/java/ro/ksza/wksp/ui/MainActivity.java
+++ app/src/main/java/ro/ksza/wksp/ui/MainActivity.java
@@ -2,32 +2,35 @@ package ro.ksza.wksp.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.TextView;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import ro.ksza.wksp.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final Logger logger = LoggerFactory.getLogger(MainActivity.class);
 
+    @Bind(R.id.hello_text_view)
+    TextView helloView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView helloView = (TextView) findViewById(R.id.hello_text_view);
-
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                helloView.setText(R.string.changed_hello_world);
-            }
-        });
+        ButterKnife.bind(this);
 
         logger.debug("MainActivity created");
+    }
+
+    @OnClick(R.id.button)
+    public void onHelloButtonClick() {
+        helloView.setText(R.string.changed_hello_world);
     }
 }
