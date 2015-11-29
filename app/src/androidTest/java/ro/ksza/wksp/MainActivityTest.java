156afdc20f2456222f5c82365343f2842ee86e0d
ksza <karoly.szanto@3pillarglobal.com>
2015-11-29 18:08:03 +0200

    Added support for automated UI testing using espresso

---
+++ app/src/androidTest/java/ro/ksza/wksp/MainActivityTest.java
@@ -0,0 +1,42 @@
+
+package ro.ksza.wksp;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void checkHelloWorldView() {
+        // Find TextView and verify the correct text that is displayed
+        onView(withId(R.id.hello_text_view))
+                .check(matches(withText(mActivityRule.getActivity().getString(R.string.hello_world))));
+    }
+
+    @Test
+    public void findButtonPerformClickAndCheckChangedText() {
+        // Find Button and Click on it
+        onView(withId(R.id.button)).perform(click());
+
+        // Find TextView and verify the correct text that is displayed
+        onView(withId(R.id.hello_text_view))
+                .check(matches(withText(mActivityRule.getActivity().getString(R.string.changed_hello_world))));
+    }
+}
--- app/src/main/java/ro/ksza/wksp/MainActivity.java
+++ app/src/main/java/ro/ksza/wksp/MainActivity.java
@@ -2,6 +2,8 @@ package ro.ksza.wksp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView helloView = (TextView) findViewById(R.id.hello_text_view);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                helloView.setText(R.string.changed_hello_world);
+            }
+        });
 
         logger.debug("MainActivity created");
     }
