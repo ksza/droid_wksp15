42fe7a27729bcc1913ada7983cb3cdae59fa2f94
ksza <karoly.szanto@3pillarglobal.com>
2015-11-29 16:56:55 +0200

    Added logging library for debug only

--- app/src/main/java/ro/ksza/wksp/MainActivity.java
+++ app/src/main/java/ro/ksza/wksp/MainActivity.java
@@ -1,13 +1,20 @@
 package ro.ksza.wksp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainActivity.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logger.debug("MainActivity created");
     }
 }
