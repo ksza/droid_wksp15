a4749a3757eeb6259d22be01da0e9ff629348584
ksza <karoly.szanto@3pillarglobal.com>
2015-11-30 20:15:03 +0200

    Integrated recycler into main activity
    - also added FAB

--- app/src/main/java/ro/ksza/wksp/ui/WishListActivity.java
+++ app/src/main/java/ro/ksza/wksp/ui/WishListActivity.java
@@ -3,7 +3,8 @@ package ro.ksza.wksp.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +19,14 @@ public class WishListActivity extends AppCompatActivity {
 
     private static final Logger logger = LoggerFactory.getLogger(WishListActivity.class);
 
-    @Bind(R.id.hello_text_view)
-    TextView helloView;
+    @Bind(R.id.movies_list)
+    MoviesRecycler moviesRecycler;
+
+    @Bind(R.id.empty_recycler)
+    View emptyRecyclerView;
+
+    private LinearLayoutManager cardListLayoutManager;
+    private MoviesAdapter moviesListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +35,17 @@ public class WishListActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        moviesListAdapter = new MoviesAdapter(this);
+        cardListLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        moviesRecycler.setLayoutManager(cardListLayoutManager);
+        moviesRecycler.setEmptyView(emptyRecyclerView);
+        moviesRecycler.setAdapter(moviesListAdapter);
+
         logger.debug("WishListActivity created");
     }
 
     @OnClick(R.id.search_button)
-    public void onHelloButtonClick() {
+    public void onSearchClick() {
         final Intent searchMovieIntent = SearchMovieActivity.createSearchMovieIntent(this);
         startActivityForResult(searchMovieIntent, SearchMovieActivity.SEARCH_MOVIE_REQUEST_CODE);
     }
