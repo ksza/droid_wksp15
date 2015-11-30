7a851b72c8a17d43134ed38b5df216fd01c5cee8
ksza <karoly.szanto@3pillarglobal.com>
2015-11-30 14:48:15 +0200

    Layout for the search activity

--- app/src/main/java/ro/ksza/wksp/ui/SearchMovieActivity.java
+++ app/src/main/java/ro/ksza/wksp/ui/SearchMovieActivity.java
@@ -2,6 +2,9 @@ package ro.ksza.wksp.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ListView;
 
 import butterknife.Bind;
@@ -12,10 +15,13 @@ import ro.ksza.wksp.R;
  * Helps search for a movie by title and displays the results in a list. Selecting
  * a movie from the list would return the selected result.
  */
-public class SearchMovieActivity extends Activity {
+public class SearchMovieActivity extends AppCompatActivity {
 
     @Bind(R.id.search_movies_list)
     ListView searchMovieList;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     private SearchMoviesAdapter moviesAdapter;
 
@@ -27,7 +33,21 @@ public class SearchMovieActivity extends Activity {
 
         ButterKnife.bind(this);
 
+        initToolbar();
+
         moviesAdapter = new SearchMoviesAdapter(this, SearchMoviesAdapter.createDummyMoviesList(20));
         searchMovieList.setAdapter(moviesAdapter);
+    }
+
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
